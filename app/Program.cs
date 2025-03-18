3b15c0fc1062599e9ffeb276e51958d8c76a1867
Saqlain <saqlainshaikh850@gmail.com>
2025-03-18 21:20:13 +0530

    added project folder

---
+++ app/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello World!");
+// Console.WriteLine(StringUtils.AreAnagramsFrequency("listen", "silent"));
+// int[] arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// ArrayUtils.LeftRotateCyclicReplacement(arr, 4);
+// Console.WriteLine(string.Join(", ", arr));
+// Console.WriteLine("[1, 2, 3, 4, 5, 6, 7, 8], 3 = " + string.Join(", ", ArrayUtils.LeftRotate([1, 2, 3, 4, 5, 6, 7, 8], 5)));
